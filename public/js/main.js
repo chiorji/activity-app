b188f17de449bc739889b228e673a9d59809be85
Chigbogu Orji <brightorji60@gmail.com>
2020-12-13 23:32:38 +0100

    Refactoring source to use singleton design pattern

--- public/js/main.js
+++ public/js/main.js
@@ -1,80 +1,77 @@
 'use strict';
- class Activity {
-    constructor (name, dueTime) {
-      this.name = name
-      this.dueTime = dueTime
-      this.id = Date.now()
-      this.completed = false
-    }
+const Activity = (function () {
+  // Retrieve items from localStorage
+  const allItems = localStorage['act'] ? JSON.parse(localStorage['act']) : []
 
-    // Return all activities saved or an empty array of no activity
-   static getAllActivities() {
+  // Returning all list sorted by id
+  const getAllActivities = () => {
+    let items = [...allItems]
     const compare = ((a,b) => b.id - a.id)
-     let act = localStorage['act'] ? JSON.parse(localStorage['act']) : [];
-     act.length > 0 ? act.sort(compare) : act
-    return act
+    items = items.length > 0 ? items.sort(compare) : items
+    return items
+  }
+
+  // Saving to localstorage
+  const saveToStorage = (array) => {
+    try {
+      const items = [...array]
+      localStorage['act'] = JSON.stringify(items)
+    } catch (err) {
+      throw new Error('Error saving activity')
+    }
+  }
+
+  // Saving new item
+  const saveItem = (description, due_time) => {
+    const regex = /^(\d{4}-\d{2}-\d{2})/
+    if (!(description || dueTime) || !(regex.test(due_time))) {
+      throw new Error('Activity description and due time are both required')
+    }
+    const item = { description, due_time, id: Date.now(), completed: false }
+    console.log('Added new item ---> ', item)
+    const items = [...allItems]
+    items.push(item)
+    saveToStorage(items)
+    return item
+  }
+
+  // Deleting an item
+  const removeById = (id) => {
+    let items = [...allItems]
+    if (typeof (id) !== "number" || !items.length) {
+      throw new Error('Validation process failed')
     }
 
-    // Add new activity t activitiess list
-    static addNewActivity(act) {
-      const all = this.getAllActivities()
-      all.push(act)
-      this.saveAllActivities(all)
+    try {
+      items = items.filter(item => parseInt(item.id) !== id)
+      saveToStorage(items)
+    } catch (err) {
+      throw err
+   }
+  }
+
+  // Changing completed status
+  const toggleCompleted = (id) => {
+    if (!id || typeof(id) !== "number") {
+      throw new Error('Id must be a number')
     }
 
-    // Save all activities to localStorage
-    static saveAllActivities(activities) {
-      try {
-          localStorage['act'] = JSON.stringify(activities)
-      } catch (err) {
-        throw new Error('Saving error')
-      }
-    }
-
-    // Removing an activity by id
-    static async removeActivityById(id) {
-      if (typeof (id) !== "number") {
-        throw new Error('Id must be a number')
-      }
-      const activities = await this.getAllActivities()
-      if (!activities.length) {
-        throw new Error('No activity to remove')
-      }
-      try {
-        const act = activities.filter(item => parseInt(item.id) !== id)
-        return act
-      } catch (err) {
-        throw new Error('Failed to delete activity with id: ' + id)
-     }
-    }
-
-   static validateItem(item) {
-      const regex = /^(\d{4}-\d{2}-\d{2})/
-      if (!item.name || (typeof (item.name) !== 'string') || item.name === ('' || 'undefined')) {
-        throw new Error('Name is required and must be a non empty string')
-      }
-
-      if (!item.dueTime || !regex.test(item.dueTime)) {
-        throw new Error('Provide a valid date in the format "yyyy-dd-mm"')
-      }
-    }
-
-  //  Mark an item as completed
-    static toggleCompleted(id) {
-      if (typeof (id) !== "number") {
-        throw new Error('Item Id must be a number')
-      }
-      try {
-        const items = this.getAllActivities()
-        for (let x = 0, max = items.length; x < max; x++) {
-          if (items[x].id === id) {
-            items[x].completed = !items[x].completed;
-            break;
-          }
-        }
-        return items
-        } catch (err) {
-          throw new Error('Failed to toggle item state')
+    const items = [...allItems]
+    for (let x = 0, max = items.length; x < max; x++) {
+      if (items[x].id === id) {
+        items[x].completed = !items[x].completed;
+        saveToStorage(items)
+        break;
       }
     }
   }
+
+  return {
+    save: saveItem,
+    get: getAllActivities,
+    remove: removeById,
+    toggle: toggleCompleted
+  }
+})()
+
+console.log(Activity)
